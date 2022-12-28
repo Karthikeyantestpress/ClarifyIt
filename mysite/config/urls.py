d7c9d383e8510034609aaa9192a8cec1f7f5d98c
Karthik <karthikeyan@testpress.in>
2022-12-28 16:23:57 +0530

    Add user authentication

--- mysite/config/urls.py
+++ mysite/config/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from mysite.clarifyit import views
 
 urlpatterns = [
+    path("", views.cover_page, name="cover_page"),
     path("admin/", admin.site.urls),
+    path("clarifyit/", include("mysite.clarifyit.urls")),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
--- mysite/mysite/clarifyit/views.py
+++ mysite/mysite/clarifyit/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def cover_page(request):
+    return render(request, "cover_page.html")
+
+
+@login_required
+def home(request):
+    return render(request, "clarifyit/home.html")
